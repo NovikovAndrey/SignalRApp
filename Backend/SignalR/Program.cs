62348480c1b09bf30e21a1228cf6e76c7e7d72f8
NovikovAndrey <Novikov.Andrey.V@hotmail.com>
2020-10-20 15:12:39 +0300

    HZ

--- Backend/SignalR/Program.cs
+++ Backend/SignalR/Program.cs
@@ -11,7 +11,7 @@ namespace SignalR
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
