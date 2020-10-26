6b21a45bfd737f8e84d20d65a5acd1e0c1c1955f
NovikovAndrey <Novikov.Andrey.V@hotmail.com>
2020-10-26 11:05:58 +0300

    Add driver

--- UITests/SignalRAppUITest/SignalRUITest.cs
+++ UITests/SignalRAppUITest/SignalRUITest.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
 
 namespace SignalRAppUITest
 {
-    class SignalRUITest
+    public class SignalRUITest
     {
+        private ChromeDriver driver;
+
+        [Fact]
+        public void ChromeDriverInitialize()
+        {
+            driver = new ChromeDriver(@".\");
+        }
     }
 }
